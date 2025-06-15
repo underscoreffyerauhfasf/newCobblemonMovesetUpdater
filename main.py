85347752c8f43b9245691cfa82e8b6a7613b0638
_FyreNova <55010147+underscoreffyerauhfasf@users.noreply.github.com>
2025-06-14 20:25:28 -0400

    minor refactor (thanks orsan!)

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import re as regex
+import pokemon_dict as pkdict
 
-targetFile = "test.txt"
 listFile = "output.txt"
 
-
-
-def findListFromLine(line):
-    pass
-
-def findStringFromLine(line):
-    return regex.sub("[^\\w\\d]","",line)
 
 
 # E is egg
@@ -19,44 +12,24 @@ def findStringFromLine(line):
 # S(number) is event
 
 
+
 class Pokemon:
-    def __init__(self,name,dex_number,forms,moveset):
+    def __init__(self,name,dex_number,formes,moveset):
         self.name = name
         self.pokedex_number = dex_number
+        self.forms = formes
         self.moveset = moveset
 
 class NewSuperCobblemonMovesetImporter:
-    def __init__(self,file):
-        self.file = file
+    def __init__(self):
         self.national_pokedex = {}
 
-        self.findPokemonData(self.file)
+        self.findPokemonData()
     
-    def findPokemonData(self,file):
-        currentPokemon = ""
+    def findPokemonData(self):
+        for i in pkdict.colonThree.keys():
+            print(i)
+            print("\n\n")
+            pass
 
-        with open(file) as openedFile:
-            for i in openedFile:
-                # count the number of indentations on the current line
-                # (this is very likely not the best way to do this but idc)
-                match i.count("\t"):
-                    # it's a pokemon, so find which one it is and store it
-                    # (but don't add it to the national dex yet!)
-                    case 1:
-                        currentPokemon = findStringFromLine(i)
-
-                        if currentPokemon == "":
-                            continue
-
-                        print(f"Current Pokemon: '{currentPokemon}'")
-
-                    # it's a data container
-                    case 2:
-                        pass
-
-                    # it's some form of data
-                    case 3:
-                        pass
-            # print(openFile.read())
-
-importer = NewSuperCobblemonMovesetImporter(targetFile)
+importer = NewSuperCobblemonMovesetImporter()
