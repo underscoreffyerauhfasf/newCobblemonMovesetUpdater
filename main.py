47c75a566ec5ab398bc90e4e9782e58b98a3cc6e
Cad48 <junkaddress417@gmail.com>
2025-06-14 21:29:46 -0400

    on pokemon object creation pass in showdown moveset and automatically get it as needed

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import pokemon_dict as pkdict
 
 listFile = "output.txt"
 
+learnsetoptions = {
+    'tm':       {'level':True,  'tm':True,  'tutor':True,   'egg':True,     'event':True},
+    'tutor':    {'level':False, 'tm':False, 'tutor':True,   'egg':False,    'event':False},
+    'egg':      {'level':False, 'tm':False, 'tutor':False,  'egg':True,     'event':False},
+}
 
+showdowntoname = {
+    'L': 'level',
+    'M': 'tm',
+    'T': 'tutor',
+    'E': 'egg',
+    'S': 'event'
+}
+
+mingen = 3
+mingenexceptions = ['nightmare']
 
 # E is egg
 # M is tm/tr
@@ -14,11 +29,36 @@ listFile = "output.txt"
 
 
 class Pokemon:
-    def __init__(self,name,dex_number,formes,moveset):
+    def __init__(self,name,dex_number,formes,showdownset):
         self.name = name
         self.pokedex_number = dex_number
         self.forms = formes
-        self.moveset = moveset
+        self.moveset = self.build_moveset(showdownset)
+    def build_moveset(self,showdownset):
+        finalmoveset = {
+            'level': [],
+            'tm': [],
+            'tutor': [],
+            'egg': []
+        }
+        for move in showdownset.keys():
+            levelfound = False
+            for method in showdownset[move]:
+                letterpos = regex.search("[A-Z]", method).span()[0]
+                gen = int(method[:letterpos])
+                param = method[letterpos+1:]
+                #add to levelset if applicable
+                if method[letterpos] != 'V' and (gen >= mingen or move in mingenexceptions):
+                    if method[letterpos] == 'L' and not levelfound:
+                        finalmoveset['level'].append((int(param),move))
+                        levelfound = True        
+                    for cobblemonmethod in learnsetoptions.keys():
+                        if learnsetoptions[cobblemonmethod][showdowntoname[method[letterpos]]] and move not in finalmoveset[cobblemonmethod]:
+                            finalmoveset[cobblemonmethod].append(move)
+        for method in finalmoveset.keys():
+            finalmoveset[method].sort()
+        return finalmoveset
+
 
 class NewSuperCobblemonMovesetImporter:
     def __init__(self):
@@ -28,7 +68,9 @@ class NewSuperCobblemonMovesetImporter:
     
     def findPokemonData(self):
         for i in pkdict.colonThree.keys():
+            thismon = Pokemon(i, 0, [], pkdict.colonThree[i]["learnset"])
             print(i)
+            print(thismon.moveset)
             print("\n\n")
             pass
 
